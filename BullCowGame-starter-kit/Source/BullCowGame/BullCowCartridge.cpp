2549fb14af221d7535c3e7f1682bb866e503cda3
garedb <garedbarney@gmail.com>
2020-06-09 17:29:31 -0700

    added basic functions and loops, mvp game code

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,75 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    SetupGame();
+
+    PrintLine(TEXT("The HiddenWord is: %s"), *HiddenWord); // Debug Line we can turn on and off  
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    if (bGameOver)
+    {
+        ClearScreen();
+        SetupGame();
+    }
+    else // Check PlayerGuess
+    {
+        ProcessGuess(Input);
+    }
+}
 
+void UBullCowCartridge::SetupGame()
+{
+    // Welcoming the player
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+    
+    HiddenWord = TEXT("cakes");
+    Lives = HiddenWord.Len();
+    bGameOver = false;
+
+    PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len());
+    PrintLine(TEXT("You have %i lives"), Lives);
+    PrintLine(TEXT("Type in your guess and press enter...")); // Prompt the player for guess
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    PrintLine(TEXT("\nPress enter to play again"));
+}
+
+void UBullCowCartridge::ProcessGuess(FString Guess)
+{
+    if (Guess == HiddenWord)
+    {
+        PrintLine(TEXT("Great guess, you win!"));
+        EndGame();
+        return;
+    }
+        // if (!IsIsogram)
+        // {
+        //     PrintLine(TEXT("No repeating letters, guess again"));
+        // }
+        
+        if (Guess.Len() != HiddenWord.Len())
+        {
+            PrintLine(TEXT("The hidden word is %i letters long"), HiddenWord.Len());
+            PrintLine(TEXT("Sorry, try guessing again! \nYou have %i lives remaining"), Lives);
+            return;
+        }
+
+        PrintLine(TEXT("Lost a life!"));
+        --Lives;
+
+        if (Lives <= 0)
+        {
+            ClearScreen();
+            PrintLine(TEXT("You have no lives left!"));
+            PrintLine(TEXT("The hidden word was: %s"), *HiddenWord);
+            EndGame();
+            return;
+        }
+    PrintLine(TEXT("Guess again, you have %i lives left"), Lives);
 }
--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
@@ -14,8 +14,13 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	public:
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString& Input) override;
+	void SetupGame();
+	void EndGame();
+	void ProcessGuess(FString Guess);
 
 	// Your declarations go below!
-	private:
-	
+	private: 
+	FString HiddenWord;
+	int32 Lives;
+	bool bGameOver;
 };
